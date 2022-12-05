53da05f697cd781a3a5f7c90d757d22957b4d24f
Leandro <leandrosiqueira.pe@gmail.com>
2022-12-05 16:29:17 -0300

    criado projeto com spring initializr. add dependencias: Spring Web, Spring Boot DevTools, H2 Database, Spring Data JPA, Spring Security, PostgreSQL Driver

---
+++ backend/src/main/java/com/lm/myagenda/MyagendaApplication.java
@@ -0,0 +1,13 @@
+package com.lm.myagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyagendaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lm/myagenda/MyagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lm.myagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
