9036170d07199981556a790042550eed30684b9c
Leandro <leandrosiqueira.pe@gmail.com>
2022-12-14 12:29:04 -0300

    Added serialVersionUID in Professional class

--- backend/src/main/java/com/lm/myagenda/models/Professional.java
+++ backend/src/main/java/com/lm/myagenda/models/Professional.java
@@ -16,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Professional implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
