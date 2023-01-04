ac05defefb96638b7e4afd3e9c3b344e7c01dfb0
Leandro <leandrosiqueira.pe@gmail.com>
2023-01-04 14:20:24 -0300

    Changed with new added mapping

--- backend/src/main/java/com/lm/myagenda/core/ModelMapperConfig.java
+++ backend/src/main/java/com/lm/myagenda/core/ModelMapperConfig.java
@@ -1,6 +1,7 @@
 package com.lm.myagenda.core;
 
 import com.lm.myagenda.dto.AddressDTO;
+import com.lm.myagenda.dto.AttendanceDTO;
 import com.lm.myagenda.dto.PersonDTO;
 import com.lm.myagenda.models.Person;
 import org.modelmapper.ModelMapper;
@@ -17,8 +18,10 @@ public class ModelMapperConfig {
         ModelMapper modelMapper = new ModelMapper();
 
         modelMapper.createTypeMap(Person.class, PersonDTO.class)
-                .addMapping(Person::getAddresses, PersonDTO::setEnderecos);
+                .addMapping(Person::getAddresses, PersonDTO::setEnderecos)
 //                .<List<AddressDTO>>addMapping(source -> source.getAddresses(),(dest, value) -> dest.setEnderecos(value));
+                .addMapping(src -> src.getAttendances(),
+                        (dest, value) -> dest.setAtendimentos((List<AttendanceDTO>) value));
         return modelMapper;
     }
 }
