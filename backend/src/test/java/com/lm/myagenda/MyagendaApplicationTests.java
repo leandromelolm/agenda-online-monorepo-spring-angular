3dafe9e2d440a08794f33b48a9d5869354578eea
Leandro <leandrosiqueira.pe@gmail.com>
2023-01-14 22:09:17 -0300

    Test main class

--- backend/src/test/java/com/lm/myagenda/MyagendaApplicationTests.java
+++ backend/src/test/java/com/lm/myagenda/MyagendaApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MyagendaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main(){
+		MyagendaApplication.main(new String[] {});
 	}
 
 }
