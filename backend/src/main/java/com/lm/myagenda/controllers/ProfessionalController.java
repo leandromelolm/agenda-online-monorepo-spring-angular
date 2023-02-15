14582e4d1d34dba477e2728d78c8788eaa876557
Leandro <leandrosiqueira.pe@gmail.com>
2023-02-15 16:15:53 -0300

    Added route findById Professional.

--- backend/src/main/java/com/lm/myagenda/controllers/ProfessionalController.java
+++ backend/src/main/java/com/lm/myagenda/controllers/ProfessionalController.java
@@ -1,25 +1,36 @@
 package com.lm.myagenda.controllers;
 
+import com.lm.myagenda.dto.ProfessionalDTO;
 import com.lm.myagenda.models.Professional;
 import com.lm.myagenda.services.ProfessionalService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value="/api")
+@RequestMapping(value="/professional")
 public class ProfessionalController {
 
     @Autowired
-    ProfessionalService pfs;
-    
-    @RequestMapping(value="/professional/all", method = RequestMethod.GET)
+    ProfessionalService service;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @RequestMapping(value="/all", method = RequestMethod.GET)
     public ResponseEntity<List<Professional>> findAll(){
-        List<Professional> professionalList = pfs.findAll();
+        List<Professional> professionalList = service.findAll();
         return ResponseEntity.ok().body(professionalList);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProfessionalDTO> getOneProfessional(@PathVariable("id") Long id){
+        Professional professional = service.findById(id);
+        ProfessionalDTO professionalDTO = modelMapper.map(professional, ProfessionalDTO.class);
+        return ResponseEntity.ok().body(professionalDTO);
+    }
+
 }
--- backend/src/main/java/com/lm/myagenda/services/ProfessionalService.java
+++ backend/src/main/java/com/lm/myagenda/services/ProfessionalService.java
@@ -2,19 +2,25 @@ package com.lm.myagenda.services;
 
 import com.lm.myagenda.models.Professional;
 import com.lm.myagenda.repositories.ProfessionalRepository;
+import com.lm.myagenda.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfessionalService {
 
     @Autowired
-    ProfessionalRepository pfr;
+    ProfessionalRepository repository;
 
     public List<Professional> findAll() {
-        return pfr.findAll();
+        return repository.findAll();
     }
-    
+
+    public Professional findById(Long id) {
+        Optional<Professional> professionalOptional = repository.findById(id);
+        return professionalOptional.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
