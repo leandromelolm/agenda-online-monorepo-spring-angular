9ebef18791e717484648b2ef3507fa8b155d5ec3
Leandro <leandrosiqueira.pe@gmail.com>
2023-02-17 23:50:24 -0300

    Create Event

--- backend/src/main/java/com/lm/myagenda/controllers/EventController.java
+++ backend/src/main/java/com/lm/myagenda/controllers/EventController.java
@@ -1,15 +1,15 @@
 package com.lm.myagenda.controllers;
 
+import com.lm.myagenda.dto.AgendaDTO;
 import com.lm.myagenda.dto.EventDTO;
 import com.lm.myagenda.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,5 +29,12 @@ public class EventController {
             @RequestParam(value="direction", defaultValue="ASC") String direction){
         Page<EventDTO> eventDTOS = service.findAllOrFindByName(name, pastDate, page, size, orderBy, direction);
         return ResponseEntity.ok().body(eventDTOS.getContent());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> create(@RequestBody EventDTO obj){
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}").buildAndExpand(service.create(obj).getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- backend/src/main/java/com/lm/myagenda/services/EventService.java
+++ backend/src/main/java/com/lm/myagenda/services/EventService.java
@@ -1,8 +1,14 @@
 package com.lm.myagenda.services;
 
 import com.lm.myagenda.dto.EventDTO;
+import com.lm.myagenda.models.Attendance;
 import com.lm.myagenda.models.Event;
+import com.lm.myagenda.models.Person;
+import com.lm.myagenda.repositories.AttendanceRepository;
 import com.lm.myagenda.repositories.EventRepository;
+import com.lm.myagenda.repositories.PersonRepository;
+import com.lm.myagenda.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class EventService {
@@ -19,7 +26,16 @@ public class EventService {
     @Autowired
     EventRepository repository;
 
-    public Page<EventDTO> findAllOrFindByName(String search, Boolean pastDate, Integer page, Integer size, String orderBy, String direction) {
+    @Autowired
+    PersonRepository personRepository;
+    @Autowired
+    AttendanceRepository attendanceRepository;
+
+    @Autowired
+    ModelMapper mapper;
+
+    public Page<EventDTO> findAllOrFindByName(
+            String search, Boolean pastDate, Integer page, Integer size, String orderBy, String direction) {
         Pageable pageable =  PageRequest.of(page, size, Sort.Direction.valueOf(direction),orderBy);
         Page<Event> events;
         if(search.isBlank() && pastDate){
@@ -32,5 +48,18 @@ public class EventService {
             events = repository.findByName(search.toUpperCase(), pageable);
         }
         return events.map(x -> new EventDTO(x));
+    }
+
+    public Event create(EventDTO obj) {
+        Optional<Person> person = personRepository.findByCpf(obj.getPersonCPF());
+        if(person.isEmpty()){
+            throw new ObjectNotFoundException("CPF não encontrado");
+        }
+        Instant dateRegister = Instant.ofEpochSecond(System.currentTimeMillis()/1000);
+        Attendance attendance = new Attendance(null, obj.getDescricao(),"Ativo",
+                obj.getDateUTC(),obj.getStart(), obj.getEnd(), "",dateRegister.toString(), person.get());
+        attendanceRepository.saveAndFlush(attendance);
+        obj.setAttendanceId(attendance.getId());
+        return repository.save(mapper.map(obj, Event.class));
     }
 }
