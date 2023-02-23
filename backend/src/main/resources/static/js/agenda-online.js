c7390c6096c388ffcf6b1247a0f1a5a1fdbdc709
Leandro <leandrosiqueira.pe@gmail.com>
2023-02-22 22:23:32 -0300

    agenda-online datepicker buttons prev and next day

--- backend/src/main/resources/static/js/agenda-online.js
+++ backend/src/main/resources/static/js/agenda-online.js
@@ -1,5 +1,10 @@
 
-var picker = $('#datePicker').datepicker({
+let daySelect = new Date();
+let formatTodayDate= formataDataParaDDMMYYYY(daySelect)
+$('#DataSelecionada').text(formatTodayDate);
+
+
+$('#datePicker').datepicker({
     showOtherMonths: true,
     selectOtherMonths: true,
     changeMonth: true,
@@ -7,8 +12,29 @@ var picker = $('#datePicker').datepicker({
     dateFormat: 'yy-mm-dd',
 
     onSelect: function (dateText, inst) {
-        var date = new Date(dateText);
+        let date = new Date(dateText);
         date.setDate(date.getDate() + 1);
+        daySelect = date;
+        $('#DataSelecionada').text(formataDataParaDDMMYYYY(date));
     },
-
 })
+
+function formataDataParaDDMMYYYY(date){
+    return ("0" + date.getDate()).slice(-2) + '/' + ("0" + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear();
+}
+
+function formataDataParaYYYYMMDD(data){
+    return data.getFullYear() + "-" + ("0" + (data.getMonth() + 1)).slice(-2) + "-" + ("0" + data.getDate()).slice(-2);
+}
+
+function diaAnterior() {
+    daySelect.setDate(daySelect.getDate() - 1)
+    $("#datePicker").datepicker("setDate", formataDataParaYYYYMMDD(daySelect));
+    $('#DataSelecionada').text(formataDataParaDDMMYYYY(daySelect));
+};
+
+function diaProximo() {
+    daySelect.setDate(daySelect.getDate() + 1)
+    $("#datePicker").datepicker("setDate", formataDataParaYYYYMMDD(daySelect));
+    $('#DataSelecionada').text(formataDataParaDDMMYYYY(daySelect));
+};
