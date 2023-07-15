40582fa8198513b170440b7356c54530a066559e
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-07-14 22:48:15 -0300

    Add event test

--- frontend/agenda-online/src/app/components/calendar/calendar.component.ts
+++ frontend/agenda-online/src/app/components/calendar/calendar.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/core';
+import { CalendarOptions, DateSelectArg } from '@fullcalendar/core';
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid'
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { FullCalendarComponent } from '@fullcalendar/angular';
+import { INITIAL_EVENTS, createEventId } from '../../data/event-utils';
 
 @Component({
   selector: 'app-calendar',
@@ -12,13 +13,13 @@ import { FullCalendarComponent } from '@fullcalendar/angular';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  
+
   calendarOptions? : CalendarOptions;
   initialview : string = "dayGridMonth";
   @ViewChild('fullcalendar') fullcalendar?: FullCalendarComponent;
 
   ngOnInit(): void {
-   this.calendarRender(this.initialview)
+    this.calendarRender(this.initialview)
   }
 
   calendarRender(initialview: string){
@@ -26,22 +27,37 @@ export class CalendarComponent implements OnInit {
       plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin],
       editable: true,
       displayEventTime: true,
-      initialView: initialview,  
+      initialView: initialview,
       headerToolbar: {
-        left: 'prev,next today',       
+        left: 'prev,next today',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
       },
-      dateClick: this.handleDateClick.bind(this),      
-    };    
+      events: INITIAL_EVENTS,
+      dateClick: this.handleDateClick.bind(this),
+    };
   }
 
   handleDateClick(arg: DateClickArg) {
-    if(arg.view.type == "dayGridMonth"){   
+    if (arg.view.type == "dayGridMonth") {
       this.changeCalendarView(arg);
     }
-    if(arg.view.type != "dayGridMonth"){   
-    console.log(arg.date);    
+    if (arg.view.type != "dayGridMonth") {
+      const title = prompt(arg.dateStr.slice(11,19) +' - Please enter a new title for your event');
+      const calendarApi = arg.view.calendar;
+      console.log(arg);
+      // calendarApi.unselect(); // clear date selection
+      let end = new Date(arg.date)
+      end.setMinutes(arg.date.getMinutes() + 30);
+
+      if (title) {
+        calendarApi.addEvent({
+          id: createEventId(),
+          title,
+          start: arg.date,
+          end: end
+        });
+      }
     }
   }
 
@@ -49,5 +65,5 @@ export class CalendarComponent implements OnInit {
     let calendar = this.fullcalendar?.getApi();
     calendar?.changeView("timeGridDay", arg.date);
   }
-
+  
 }
