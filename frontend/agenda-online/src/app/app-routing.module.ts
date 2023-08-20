4b69f4cbd9517668ef8ac0d9d9173c90ffe2c14e
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-08-20 18:12:21 -0300

    refactor(app-routing.module): add new route

--- frontend/agenda-online/src/app/app-routing.module.ts
+++ frontend/agenda-online/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './components/calendar/calendar.component';
+import { AgendaComponent } from './pages/agenda/agenda.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'calendar', pathMatch: 'full'},
+  {path: '', redirectTo: 'agenda', pathMatch: 'full'},
+  {path: 'agenda', component: AgendaComponent},
   {path: 'calendar', component: CalendarComponent},
-  {path: '**', redirectTo: 'calendar'}
+  {path: '**', redirectTo: 'agenda'}
 ];
 
 @NgModule({
