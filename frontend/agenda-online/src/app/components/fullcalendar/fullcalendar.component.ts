11eb2113fae6dec1a70ad4bee1f2fb23251bd12f
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-08-21 17:16:19 -0300

    feat: page fullcalendar

--- frontend/agenda-online/src/app/components/fullcalendar/fullcalendar.component.ts
+++ frontend/agenda-online/src/app/components/fullcalendar/fullcalendar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import {CalendarOptions} from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 
 @Component({
   selector: 'app-fullcalendar',
   templateUrl: './fullcalendar.component.html',
   styleUrls: ['./fullcalendar.component.css']
 })
-export class FullcalendarComponent {
+export class FullcalendarComponent implements OnInit {
+  
+  @ViewChild('fullcalendar') fullcalendar?: FullCalendarComponent; 
+  calendarOptions? :CalendarOptions;
 
+  ngOnInit(): void {
+    this.renderFullCalendar()
+  }
+
+  renderFullCalendar(){
+    this.calendarOptions = {
+      plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin],
+      initialView: 'timeGridDay'
+    }
+  }
 }
